dc27ce53d4fb0b78f8366449f70dd4aff4b01a77
serichannu <serina.chappy@icloud.com>
2023-12-22 18:38:57 +0900

    3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\GoalController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,3 +22,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('goals', GoalController::class)->only(['index','store', 'update', 'destroy'])->middleware('auth');
+
+Route::resource('goals.todos', TodoController::class)->only('store', 'update', 'destroy')->middleware('auth');
