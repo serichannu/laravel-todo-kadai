d424e87b492b19577280479e61832094daebc09b
serichannu <serina.chappy@icloud.com>
2023-12-22 16:52:15 +0900

    2

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [GoalController::class, 'index']);
+Route::get('/', [GoalController::class, 'index'])->middleware('auth');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('goals', GoalController::class)->only(['index','store', 'update', 'destroy']);
+Route::resource('goals', GoalController::class)->only(['index','store', 'update', 'destroy'])->middleware('auth');
